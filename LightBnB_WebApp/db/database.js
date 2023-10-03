793991944f6a079e236210a310aa6b48b14459e0
Diana Ichmoukhametov <akhtyamovadiana.gmail.com>
2023-10-02 22:56:19 -0400

    Refactor the getAllReservations function to return correct data

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -27,7 +27,6 @@ const getUserWithEmail = function(email) {
     .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
       if (result) {
-        console.log(result.rows[0]);
         return result.rows[0];
       } else {
         return null;
@@ -50,7 +49,6 @@ const getUserWithId = function(id) {
     .query(`SELECT * FROM users WHERE id = $1`, [id])
     .then((result) => {
       if (result) {
-        console.log(result.rows[0]);
         return result.rows[0];
       } else {
         return null;
@@ -73,6 +71,9 @@ const addUser = function(user) {
   RETURNING *;`, [user.name, user.email, user.password])
     .then((result) => {
       return result;
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 };
 
@@ -84,7 +85,23 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `, [guest_id, limit])
+  .then((result) => {
+    return result.rows
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /// Properties
